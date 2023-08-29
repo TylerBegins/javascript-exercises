c5cad8d310c9e3e1f4ff550bb455be82c8833dd4
Tyler W <74345181+TylerBegins@users.noreply.github.com>
2023-08-28 21:46:13 -0700

    solved problem 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    const ctemp = Math.round(((temp - 32) * (5/9)) * 10) / 10;
+    return ctemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    const ftemp = Math.round(((temp * 9/5) + 32) * 10) / 10;
+    return ftemp;
 };
 
 // Do not edit below this line
