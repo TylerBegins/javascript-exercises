6f1a779cd8f4351497fc10790b4c302ab6bb0866
Tyler W <74345181+TylerBegins@users.noreply.github.com>
2023-08-29 08:23:22 -0700

    problem 09 ongoing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (palin) {
+    
+    let revPalin = palin.split("").reverse().join("");
+    if (palin === revPalin) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
