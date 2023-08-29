35329c84db4de9e8634cb8fcf2a328a1adbe5dd7
Tyler W <74345181+TylerBegins@users.noreply.github.com>
2023-08-28 21:23:54 -0700

    solved problems

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...indexItem) {
+    const args = array.indexOf(indexItem);
+    
+    args.splice(index, 1); 
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    let sum = 0;
 
+    if (arg1 !== parseInt(arg1) || arg2 !== parseInt(arg2)) {
+        return "ERROR";
+    }
+        
+    if (arg1 < 0 || arg2 < 0) {
+        return "ERROR";
+    } else {
+        if (arg1 >= arg2) {
+            for (i = arg2; i <= arg1; i++) {
+                sum += i;
+            };
+        } else {
+        for (i = arg1; i <= arg2; i++) {
+            sum += i;
+    };}
+};
+    return sum;
 };
 
 // Do not edit below this line
