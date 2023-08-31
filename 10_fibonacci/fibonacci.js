fb0cde40fa381aadd7565c3ec905a362beb4a65c
Tyler W <74345181+TylerBegins@users.noreply.github.com>
2023-08-30 20:29:34 -0700

    problem 10 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,37 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let x = 0;
+    let y = 1;
+    let z = 1;
+    let intNum = Number(num);
+    if (intNum < 0) {
+        return "OOPS";
+    };
+    if (intNum === 1) {
+        return 1;
+    };
+    for (i=1; i < intNum; i++) {
+        x += y;
+        y = z;
+        z = x;
+    };
+    return x;
+
 
 };
+
+/*
+    x starts at 1
+    y starts at 0
+    for each iteration:
+        y is increased by x
+        y's value is assigned to x
+    1 1 2 3 5 8 13 21 34 55
+
+*/
+
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
